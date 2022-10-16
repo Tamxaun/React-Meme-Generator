62af0fcf73bac54074dc2c7fe2af447a6287c298
Dmitry <deus.main@gmail.com>
2022-10-17 00:10:49 +0300

    add get random img from local dataset

--- src/MemeForm/MemeForm.tsx
+++ src/MemeForm/MemeForm.tsx
@@ -1,22 +1,38 @@
-import { FC } from 'react';
+import { FC, MouseEvent, useState } from 'react';
 import { MemeFormProps } from '.';
+import memeData from './memesData';
 import styles from './MemeForm.module.css';
 
 export const MemeForm: FC<MemeFormProps> = (props) => {
+	let [memeImage, setMemeImagem] = useState('');
+
+	function getMemeImage(event: MouseEvent<HTMLButtonElement>) {
+		event.preventDefault();
+		let randomImageUrl: string = memeData.data.memes[Math.floor(Math.random() * memeData.data.memes.length)].url;
+		setMemeImagem(randomImageUrl)
+		console.log('Button was clicked');
+		console.log(randomImageUrl);
+	}
+
 	return (
 		<main className={styles.wrap}>
 			<form className={styles.form} action="" method="post">
 				<fieldset className={styles.fieldset}>
-					<input className={styles.input} type="text" placeholder="upper text" />
-					<input className={styles.input} type="text" placeholder='below text' />
+					<input className={styles.input} type="text" placeholder="Upper text" />
+					<input className={styles.input} type="text" placeholder='Below text' />
 				</fieldset>
 				<p>
-					<button className={styles.button} type="submit">
+					<button onClick={getMemeImage} className={styles.button} type="submit">
 						<span>Get a new meme image</span>
 						<span className={styles.buttonIcon}>🖼</span>
 					</button>
 				</p>
 			</form>
+			<div>
+				<figure className={styles.imageWrap}>
+					{memeImage.length ? <img src={memeImage} alt="meme image" /> : <figcaption>To get a meme, click the button ⇡</figcaption>}
+				</figure>
+			</div>
 		</main>
 	);
 };
